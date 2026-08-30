d4256d37d08510f7ca3745e8300dbe22ec3ab3cd
William Crane <wcran7@gmail.com>
2026-08-30 15:38:35 +1000

    Feat(Get one Student): Retrieve one student from the list in students.py

--- FASTAPI/04_School_Management_API/app/routers/students.py
+++ FASTAPI/04_School_Management_API/app/routers/students.py
@@ -2,8 +2,40 @@ from fastapi import APIRouter, HTTPException, status
 
 from app.schemas.student import Student, StudentCreate, StudentUpdate
 
+students = [
+    {
+        "id": 1,
+        "first_name": "Ash",
+        "last_name": "Ketchum",
+        "year_level": 10,
+        "email": "ash@example.com"
+    },
+    {
+        "id": 2,
+        "first_name": "Misty",
+        "last_name": "Waterflower",
+        "year_level": 9,
+        "email": "misty@example.com"
+    }
+]
+
+@router.get("/", response_model=list[Student])
+def get_students():
+    return students 
 
 router = APIRouter(
     prefix="/api/v1/students",
     tags=["Students"]
 )
+
+@router.get("/{student_id}", response_model=Student)
+def get_student(student_id: int):
+
+    for student in students:
+        if student["id"] == student_id:
+            return student
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Student not found"
+    )
